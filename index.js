a26c8a95f9903d541410eec1f43623ae7eff981b
JamesSarkissian <jamessark07@gmail.com>
2025-11-12 18:35:24 -0500

    not done
    Add active state to prevent multiple clicks.

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ let btn = document.getElementById("spin");
 let number = Math.ceil(Math.random() * 1000);
 
 let clicks = 0;
+let active = false;
 btn.onclick = function () {
   clicks += 1;
-  if(clicks == 1 ){
+
+	if(active){
+		return;
+	}
 	container.style.transform = "rotate(" + number + "deg)";
 	number += Math.ceil(Math.random() * 1000);
-  }
+	active = false;
 }
