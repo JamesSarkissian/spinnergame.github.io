1fab3ab8ef5884893c3f9d309b8632735184e36e
JamesSarkissian <jamessark07@gmail.com>
2025-11-18 14:21:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,21 +6,21 @@ let clicks = 0;
 let active = false;
 
 const degreeTable = [
-  { section: 1, start: 337.5, end: 22.5 },
-  { section: 2, start: 22.5,  end: 67.5 },
-  { section: 3, start: 67.5,  end: 112.5 },
-  { section: 4, start: 112.5, end: 157.5 },
-  { section: 5, start: 157.5, end: 202.5 },
-  { section: 6, start: 202.5, end: 247.5 },
-  { section: 7, start: 247.5, end: 292.5 },
-  { section: 8, start: 292.5, end: 337.5 },
+  { Amount: 1,000,000, start: 337.5, end: 22.5 },
+  { Amount: 5, start: 22.5,  end: 67.5 },
+  { Amount: 20, start: 67.5,  end: 112.5 },
+  { Amount: 10, start: 112.5, end: 157.5 },
+  { Amount: 100, start: 157.5, end: 202.5 },
+  { Amount: 10, start: 202.5, end: 247.5 },
+  { Amount: 20, start: 247.5, end: 292.5 },
+  { Amount: 5, start: 292.5, end: 337.5 },
 ];
 
 function getSection(spinDegree) {
   const normalized = spinDegree % 360;
 
   for (let i = 0; i < degreeTable.length; i++) {
-    const { section, start, end } = degreeTable[i];
+    const { Amount, start, end } = degreeTable[i];
 
     if (start > end) {
       // Wrap-around case (section 1)
